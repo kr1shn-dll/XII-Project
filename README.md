7856a720fc47a7e9b9dc6274c5741a5f3293a7be
Krishn Sharma <sharmakrishn69@gmail.com>
2024-09-29 01:34:11 +0530

    creating the repo

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# The Continental hotel management tool
+
+This is a simple school cs project for my grade 12 board examinations.
+
+
+
+## Deployment
+
+To run this project open the main.py python file
+
+```bash
+  python main.py
+```
+
+A few MYSQL commands need to be excuted 
+
+```bash
+  resources/SQL_cmds.sql
+  ```
+Make sure to install the required font(s) in the resources folder
+
+
+
+
+## Features
+
+- Interactive and smiple GUI
+- Add customer details
+- Delete customer information
+- Edit customer information
+- View customers
+- Add room details
+- Delete rooms
+- Book rooms 
+- View rooms
---
+++ view_customers.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import mysql.connector as sql
+
+
+connection = sql.connect(host="localhost",
+                                 user="root",
+                                 passwd="root",
+                                 database="project",
+                                 auth_plugin='mysql_native_password')
+
+cursor = connection.cursor()
+
+window = Tk()
+
+bg = PhotoImage(file=r"Resources\bg.png")
+logo = PhotoImage(file=r"Resources\logo.png")
+
+window.geometry("1280x720")
+window.configure(bg = "#FFFFFF")
+window.title("CONTINENTAL HOTEL |  CUSTOMER INFORMATION ")
+window.iconphoto(True,logo)
+
+canvas = Canvas(window,bg = "#FFFFFF",height = 720,width = 1280,highlightthickness = 0)
+canvas.place(x = 0, y = 0)
+
+canvas.create_image(640,360,image=bg)
+canvas.create_text(47,0,anchor="nw",text="THE CONTINENTAL",fill="#950B3C",font=("Cinzel Decorative Black", 106 * -1))
+
+
+window.resizable(False,False)
+window.mainloop()
